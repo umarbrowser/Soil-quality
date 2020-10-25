347249fe1f7a6bedca3203b220364dc31d3ff96c
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 16:03:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, request, render_template #import main Flask class and request object
+import model_predict as M
 
 app = Flask(__name__) #create the Flask app
 
@@ -7,6 +8,23 @@ app = Flask(__name__) #create the Flask app
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('/index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    print("Entered")
+    if request.method == 'POST':
+        print("Entered here")
+        file = request.files['image'] # fet input
+        filename = file.filename        
+        print("@@ Input posted = ", filename)
+        
+        file_path = os.path.join('static/user uploaded', filename)
+        file.save(file_path)
+
+        print("@@ Predicting class......")
+        pred, output_page = M.model_predict(file_path,SoilNet)
+              
+        return render_template(output_page, pred_output = pred, user_image = file_path)
 
 
 if __name__ == '__main__':
