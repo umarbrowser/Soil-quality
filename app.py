f6d265516aea5bb5f0bc47adf4157c20a7ed49c9
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 15:05:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, request, render_template
-from model_predict import model_predict
+import os
+from flask import Flask, render_template
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    APP_PATH = os.path.dirname(__file__)
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('/index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
