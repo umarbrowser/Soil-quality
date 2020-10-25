69c153630584ecb4751f5dcb83c534d945be394d
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 15:42:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask import Flask, request, render_template #import main Flask class and r
 app = Flask(__name__) #create the Flask app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('/index.html')
 
 
-if _name == '__main_':
+if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
