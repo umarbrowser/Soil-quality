17a30413a9a8ef8af5f990b617e3a05fffdcda77
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 13:26:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -114,26 +114,9 @@ def model_predict(image_path,model):Regur Soil
     
 
 @app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html')
-
-"""
-@app.route('/predict',methods=['GET','POST'])
-def predict():
-    print("Entered")
-    if request.method == 'POST':
-        print("Entered here")
-        file = request.files['image'] # fet input
-        filename = file.filename        
-        print("@@ Input posted = ", filename)
-        
-        file_path = os.path.join('static/user uploaded', filename)
-        file.save(file_path)
-        print("@@ Predicting class......")
-        pred, output_page = model_predict(file_path,SoilNet)
-              
-        return render_template("index.html", pred_output = pred, user_image = file_path)"""
 
 if __name__ == '__main__':
     app.run(debug=True)
