f958b48d8dedc55cd34d879479fe5824e4dd2cef
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 12:04:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,12 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True,threaded=False)"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+    
     
