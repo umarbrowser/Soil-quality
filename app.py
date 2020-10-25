ae8b98f16017c69709a0d068f2c6f5c44b56b757
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 20:36:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
-
-from flask import Flask, request, render_template #import main Flask class and request object
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
+
 
 import os, sys, glob, re
 
-app = Flask(__name__) #create the Flask app
-app.config.update(
-    TESTING=True,
-    SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/'
-)
-model_path = "model.h5"
+app = Flask(__name__)
+
+model_path = "SoilNet_92_89_4.h5"
 
 SoilNet = load_model(model_path)
 
-classes = {0:"Alluvial Soil can produce ( Rice,Wheat,Sugarcane,Maize,Cotton,Soyabean,Jute )",1:"Regular Soil can produce ( Virginia, Wheat , Jowar,Millets,Linseed,Castor,Sunflower) ",2:"Clay Soil can produce ( Rice,Lettuce,Chard,Broccoli,Cabbage,Snap Beans )",3:"Laterite soil can produce (Cotton,Wheat,Pilses,Millets,OilSeeds,Potatoes )"}
+classes = {0:"Alluvial Soil:-{ Rice,Wheat,Sugarcane,Maize,Cotton,Soyabean,Jute }",1:"Black Soil:-{ Virginia, Wheat , Jowar,Millets,Linseed,Castor,Sunflower} ",2:"Clay Soil:-{ Rice,Lettuce,Chard,Broccoli,Cabbage,Snap Beans }",3:"Red Soil:{ Cotton,Wheat,Pilses,Millets,OilSeeds,Potatoes }"}
 
-def model_predict(image_path,model):Regur Soil
+def model_predict(image_path,model):
     print("Predicted")
     image = load_img(image_path,target_size=(150,150))
     image = img_to_array(image)
@@ -30,31 +27,32 @@ def model_predict(image_path,model):Regur Soil
     
     
     if result == 0:
-        print("Alluvial_soil.html")
+        print("Alluvial.html")
         
-        return "Alluvial","Alluvial_soil.html"
+        return "Alluvial","Alluvial.html"
     elif result == 1:
-        print("Regular_soil.html")
+        print("Black.html")
         
-        return "Regular Soil", "Regular_soil.html"
+        return "Black", "Black.html"
     elif result == 2:
-        print("Clay_soil.html")
+        print("Clay.html")
         
-        return "Clay Soil" , "Clay_soil.html"
+        return "Clay" , "Clay.html"
     elif result == 3:
-        print("Laterite_soil.html")
+        print("Red.html")
         
-        return "Laterite Soil" , "Laterite_soil.html"
+        return "Red" , "Red.html"
+    
 
-@app.route('/')
-@app.route('/index', methods=['GET'])
+
+@app.route('/',methods=['GET'])
 def index():
-    return render_template('/index.html')
+    return render_template('index.html')
+
+
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
-    return render_template('Alluvial_soil.html')
-'''
     print("Entered")
     if request.method == 'POST':
         print("Entered here")
@@ -66,11 +64,12 @@ def predict():
         file.save(file_path)
 
         print("@@ Predicting class......")
-        pred, output_page = M.model_predict(file_path,SoilNet)
+        pred, output_page = model_predict(file_path,SoilNet)
+              
         return render_template(output_page, pred_output = pred, user_image = file_path)
-'''
-             
-        
+    
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    app.run(debug=True,threaded=False)
+    
