e5ef3423e90c51e5d01afe613df3ff7cf41f4d36
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 15:13:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import os
 from flask import Flask, render_template
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    APP_PATH = os.path.dirname(__file__)
-
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('/index.html', form=form)
+    return render_template('/index.html')
 
 
 if __name__ == '__main__':
