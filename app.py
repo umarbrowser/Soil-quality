bd1a1a9987b33b342b8d8c09fd9c28aad99a61f8
umar abdullahi <umarbrowser20@gmail.com>
2020-10-25 16:06:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__) #create the Flask app
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('/index.html')
-
+'''
 @app.route('/predict',methods=['GET','POST'])
 def predict():
     print("Entered")
@@ -24,7 +24,7 @@ def predict():
         print("@@ Predicting class......")
         pred, output_page = M.model_predict(file_path,SoilNet)
               
-        return render_template(output_page, pred_output = pred, user_image = file_path)
+        return render_template(output_page, pred_output = pred, user_image = file_path)'''
 
 
 if __name__ == '__main__':
